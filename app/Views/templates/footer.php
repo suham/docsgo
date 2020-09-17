89264c744ac232dfb6332d88f9ba8ebb8d64a81d
sudhanshu tiwari <sudhanshu@viosmedical.com>
2020-09-17 14:47:47 +0530

    Added Sidemenu

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,5 +1,46 @@
-<script src="/assets/js/jquery-3.2.1.slim.min.js" ></script>
-<script src="/assets/js/popper.min.js" ></script>
-<script src="/assets/js/bootstrap.min.js"  ></script>
+</main>
+
+</div>
+<!-- <script src="/assets/js/jquery-3.2.1.slim.min.js"></script> -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="/assets/js/popper.min.js"></script>
+<script src="/assets/js/bootstrap.min.js"></script>
+<script>
+   
+
+        $(".sidebar-dropdown > a").click(function() {
+        $(".sidebar-submenu").slideUp(200);
+        if (
+        $(this)
+        .parent()
+        .hasClass("active")
+        ) {
+        $(".sidebar-dropdown").removeClass("active");
+        $(this)
+        .parent()
+        .removeClass("active");
+        } else {
+        $(".sidebar-dropdown").removeClass("active");
+        $(this)
+        .next(".sidebar-submenu")
+        .slideDown(200);
+        $(this)
+        .parent()
+        .addClass("active");
+        }
+        });
+
+        $("#close-sidebar").click(function() {
+        $(".page-wrapper").removeClass("toggled");
+        });
+        $("#show-sidebar").click(function() {
+        $(".page-wrapper").addClass("toggled");
+        });
+
+
+
+
+</script>
 </body>
+
 </html>
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/header.css">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
     <title></title>
 
   </head>
@@ -12,7 +14,7 @@
     <?php
       $uri = service('uri');
      ?>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
       <a class="navbar-brand" href="/">PRT</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -45,4 +47,152 @@
         <?php endif; ?>
       </div>
       </div>
+    </nav> -->
+    <div class="page-wrapper chiller-theme toggled">
+    <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
+      <i class="fas fa-bars"></i>
+    </a>
+    <?php if (session()->get('isLoggedIn')): ?>
+    <nav id="sidebar" class="sidebar-wrapper">
+      <div class="sidebar-content">
+        <div class="sidebar-brand">
+          <a href="#" title="Project Data Reporting Tool">Project Reporting Tool</a>
+          <div id="close-sidebar">
+            <i class="fas fa-times"></i>
+          </div>
+        </div>
+        <div class="sidebar-header">
+          <div class="user-pic">
+          <a href="/profile" title="My Profile">
+          <img class="img-responsive img-rounded" src="https://raw.githubusercontent.com/azouaoui-med/pro-sidebar-template/gh-pages/src/img/user.jpg"
+              alt="User picture">
+        </a>
+            
+          </div>
+          <div class="user-info">
+            <span class="user-name">
+              <strong><?= session()->get('name') ?></strong>
+            </span>
+           
+          </div>
+        </div>
+       
+        <!-- sidebar-search  -->
+        <div class="sidebar-menu">
+          <ul>
+            <li class="header-menu">
+              <span>Manage</span>
+            </li>
+            <li class="sidebar-dropdown">
+              <a href="#">
+                <i class="fa fa-briefcase"></i>
+                <span>Project</span>
+                <span class="badge badge-pill badge-warning">New</span>
+              </a>
+              <div class="sidebar-submenu">
+                <ul>
+                  <li>
+                    <a href="#">Add Project</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="sidebar-dropdown">
+              <a href="#">
+                <i class="fa fa-folder-open"></i>
+                <span>Documents</span>
+                <span class="badge badge-pill badge-warning">New</span>
+              </a>
+              <div class="sidebar-submenu">
+                <ul>
+                  <li>
+                    <a href="#">Add Documents</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            
+            <li class="sidebar-dropdown">
+              <a href="#">
+                <i class="far fa-user"></i>
+                <span>Team</span>
+                <span class="badge badge-pill badge-warning">New</span>
+              </a>
+              <div class="sidebar-submenu">
+                <ul>
+                  <li>
+                    <a href="#">Add Member</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="sidebar-dropdown">
+              <a href="#">
+                <i class="far fa-edit"></i>
+                <span>Update from Testlink</span>
+                
+              </a>
+              <div class="sidebar-submenu">
+                <ul>
+                  <li>
+                    <a href="#">Requirements</a>
+                  </li>
+                  <li>
+                    <a href="#">Test Cases</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="header-menu">
+              <span>Reports </span>
+              
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-book"></i>
+                <span>Traceability Matrix</span>
+                <span class="badge badge-pill badge-primary">Beta</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-list"></i>
+                <span>IEC 62304 Compliance</span>
+                
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-folder"></i>
+                <span>Cybersecurity Compliance</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <!-- sidebar-menu  -->
+      </div>
+      <!-- sidebar-content  -->
+      <div class="sidebar-footer">
+        <!-- <a href="#">
+          <i class="fa fa-bell"></i>
+          <span class="badge badge-pill badge-warning notification">3</span>
+        </a>
+        <a href="#">
+          <i class="fa fa-envelope"></i>
+          <span class="badge badge-pill badge-success notification">7</span>
+        </a>
+        <a href="#">
+          <i class="fa fa-cog"></i>
+          <span class="badge-sonar"></span>
+        </a> -->
+        <a href="/logout">
+          <i class="fa fa-power-off"></i>
+        </a>
+      </div>
     </nav>
+    <main class="page-content">
+    <?php endif; ?>
+    
+      
+
+      
