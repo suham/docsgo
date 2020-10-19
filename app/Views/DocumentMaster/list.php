cba315b8ec48172122f8b8be6505966c930901b3
sudhanshu tiwari <sudhanshu@viosmedical.com>
2020-10-19 15:01:49 +0530

    Fixed alignment from References and Traceability pages

--- app/Views/DocumentMaster/list.php
+++ app/Views/DocumentMaster/list.php
@@ -8,37 +8,39 @@
 
   <?php else: ?>
     <div class="table-responsive">
-      <table class="table table-striped table-hover"  id="document-master-list">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Category</th>
-            <th scope="col">Version</th>
-            <th scope="col">Status</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody class="bg-white ">
-          <?php foreach ($data as $key=>$row): ?>
-              <tr scope="row" id="<?php echo $row['id'];?>">
-                  <td><?php echo $key+1; ?></td>
-                  <td><?php echo $row['name'];?></td>
-                  <td><?php echo $row['category'];?></td>
-                  <td><?php echo $row['version'];?></td>
-                  <td><?php echo $row['status'];?></td>
-                  <td>
-                      <a href="/documents-master/add/<?php echo $row['id'];?>" class="btn btn-warning">
-                          <i class="fa fa-edit"></i>
-                      </a>
-                      <a onclick="deleteDocument(<?php echo $row['id'];?>)" class="btn btn-danger ml-2">
-                          <i class="fa fa-trash text-light"></i>
-                      </a>
-                  </td>
+      
+        <table class="table table-striped table-hover"  id="document-master-list">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Category</th>
+                <th scope="col">Version</th>
+                <th scope="col">Status</th>
+                <th scope="col">Actions</th>
               </tr>
-          <?php endforeach; ?>
-        </tbody>
-      </table>
+            </thead>
+            <tbody class="bg-white ">
+              <?php foreach ($data as $key=>$row): ?>
+                  <tr scope="row" id="<?php echo $row['id'];?>">
+                      <td><?php echo $key+1; ?></td>
+                      <td><?php echo $row['name'];?></td>
+                      <td><?php echo $row['category'];?></td>
+                      <td><?php echo $row['version'];?></td>
+                      <td><?php echo $row['status'];?></td>
+                      <td>
+                          <a href="/documents-master/add/<?php echo $row['id'];?>" class="btn btn-warning">
+                              <i class="fa fa-edit"></i>
+                          </a>
+                          <a onclick="deleteDocument(<?php echo $row['id'];?>)" class="btn btn-danger ml-2">
+                              <i class="fa fa-trash text-light"></i>
+                          </a>
+                      </td>
+                  </tr>
+              <?php endforeach; ?>
+            </tbody>
+          </table>
+      
     </div>
 
 <?php endif; ?>
@@ -49,8 +51,8 @@
   $(document).ready( function () {
     $('#document-master-list').DataTable({
       "responsive": true,
-      "scrollX": true,
       "fixedHeader": true,
+      "autoWidth": false
     });
   });
 
--- app/Views/TraceabilityMatrix/list.php
+++ app/Views/TraceabilityMatrix/list.php
@@ -1,18 +1,5 @@
 
-<div class="container1">
-<!-- <div class="row">
-      <div class="col-12">
-          <div class="form-group" readonly="readonly">
-
-              <input type="radio" name="issues-status-type" id="TraceabilityRDBtn1" 
-              onclick="javascript:window.location.href='/traceability-matrix/view/1';" <?php //echo ($checkedVals['TraceabilityRDBtn1']) == 1 ? "checked" : ""; ?> /> List 
-              <input type="radio" name="issues-status-type" id="TraceabilityRDBtn2" 
-              onclick="javascript:window.location.href='/traceability-matrix/view/2';"  <?php //echo ($checkedVals['TraceabilityRDBtn2']) == 1 ? "checked" : ""; ?> /> Gaps
-
-          </div>
-      </div>
-    </div> -->
-
+<div class="container">
 <?php if (count($data) == 0): ?>
 
   <div class="alert alert-warning" role="alert">
@@ -21,100 +8,108 @@
 
   <?php else: ?>
 
-    <?php if (($listView)): ?>
-    <table class="table table-striped table-hover table-responsive" id="table1" >
-      <thead >
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">User Needs</th>
-          <th scope="col">System</th>
-          <th scope="col">Subsystem</th>
-          <th scope="col">Test</th>
-          <th scope="col">Design</th>
-          <th scope="col">Code</th>
-          <!-- <th scope="col" style="width:125px">Update Date</th> -->
-          <th scope="col" style="width:125px">Action</th>
-        </tr>
-      </thead>
-      <tbody  class="bg-white">
-        <?php foreach ($data as $key=>$row): ?>
-            <tr scope="row" id="<?php echo $row['id'];?>">
-                <td><?php echo $key+1; ?></td>
-                <td><?php echo $row['User Needs'];?></td>
-                <td>
-                  <div>
-                  <?php foreach ($row['System'] as $key1=>$row1 ): ?>
-                  <div>
-                    <div><?php echo $row1['requirement'];?></div>
-                  </div>
-                  <?php endforeach; ?>
-                  </div>
-                </td>
-                <td>
-                  <div>
-                  <?php foreach ($row['Subsystem'] as $key1=>$row1 ): ?>
-                  <div>
-                    <div><?php echo $row1['requirement'];?></div>
-                  </div>
-                  <?php endforeach; ?>
-                  </div>
-                </td>
-                <td>
-                  <div>
-                  <?php foreach ($row['testcase'] as $key1=>$row1 ): ?>
-                  <div>
-                    <div><?php echo $row1['requirement'];?></div>
-                  </div>
-                  <?php endforeach; ?>
-                  </div>
-                </td>
-                <td><?php echo $row['design']; ?></td>
-                <td><?php echo $row['code'];?></td>
-                <td>
-                    <a href="/traceability-matrix/add/<?php echo $row['id'];?>" class="btn btn-warning">
-                        <i class="fa fa-edit"></i>
-                    </a>
-                    <?php if (session()->get('is-admin')): ?>
-                    <a onclick="deleteTraceabilityMatrix(<?php echo $row['id'];?>)" class="btn btn-danger ml-2">
-                        <i class="fa fa-trash text-light"></i>
-                    </a>
-                    <?php endif; ?>
-                </td>
+  <?php if (($listView)): ?>
+    <div class="table-responsive">
+      
+        <table class="table table-striped table-hover" id="table1" >
+          <thead >
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">User Needs</th>
+              <th scope="col">System</th>
+              <th scope="col">Subsystem</th>
+              <th scope="col">Test</th>
+              <th scope="col">Design</th>
+              <th scope="col">Code</th>
+              <!-- <th scope="col" style="width:125px">Update Date</th> -->
+              <th scope="col" style="width:125px">Action</th>
             </tr>
-        <?php endforeach; ?>
-      </tbody>
-    </table>
-    <?php endif; ?>
+          </thead>
+          <tbody  class="bg-white">
+            <?php foreach ($data as $key=>$row): ?>
+                <tr scope="row" id="<?php echo $row['id'];?>">
+                    <td><?php echo $key+1; ?></td>
+                    <td><?php echo $row['User Needs'];?></td>
+                    <td>
+                      <div>
+                      <?php foreach ($row['System'] as $key1=>$row1 ): ?>
+                      <div>
+                        <div><?php echo $row1['requirement'];?></div>
+                      </div>
+                      <?php endforeach; ?>
+                      </div>
+                    </td>
+                    <td>
+                      <div>
+                      <?php foreach ($row['Subsystem'] as $key1=>$row1 ): ?>
+                      <div>
+                        <div><?php echo $row1['requirement'];?></div>
+                      </div>
+                      <?php endforeach; ?>
+                      </div>
+                    </td>
+                    <td>
+                      <div>
+                      <?php foreach ($row['testcase'] as $key1=>$row1 ): ?>
+                      <div>
+                        <div><?php echo $row1['requirement'];?></div>
+                      </div>
+                      <?php endforeach; ?>
+                      </div>
+                    </td>
+                    <td><?php echo $row['design']; ?></td>
+                    <td><?php echo $row['code'];?></td>
+                    <td>
+                        <a href="/traceability-matrix/add/<?php echo $row['id'];?>" class="btn btn-warning">
+                            <i class="fa fa-edit"></i>
+                        </a>
+                        <?php if (session()->get('is-admin')): ?>
+                        <a onclick="deleteTraceabilityMatrix(<?php echo $row['id'];?>)" class="btn btn-danger ml-2">
+                            <i class="fa fa-trash text-light"></i>
+                        </a>
+                        <?php endif; ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+          </tbody>
+        </table>
+      
+    </div>
+  <?php endif; ?>
 
-<?php if (($gapView)): ?>
-  <table class="table table-striped table-hover table-responsive"  id="table2" >
-  <thead >
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Category</th>
-      <th scope="col">Name</th>
-      <th scope="col">Description</th>
-    </tr>
-  </thead>
-  <tbody  class="bg-white">
-    
-  </tbody>
-</table>
-<?php endif; ?>
-<?php endif; ?>
-  
+  <?php if (($gapView)): ?>
+    <div class="table-responsive">
+      
+        <table class="table table-striped table-hover"  id="table2" >
+        <thead >
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Category</th>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+          </tr>
+        </thead>
+        <tbody  class="bg-white"></tbody>
+        </table>
+      
+    </div>
+  <?php endif; ?>
+
+
+
+
+  <?php endif; ?>
 </div>
-
 <script>
 $(document).ready( function () {
     $('#table1').DataTable({
       "responsive": true,
-      "scrollX": true,
+      "autoWidth": false,
       "fixedHeader": true,
     });
     $('#table2').DataTable({
       "responsive": true,
-      "scrollX": true,
+      "autoWidth": false,
       "fixedHeader": true,
     });
 });
