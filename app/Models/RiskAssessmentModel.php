da3c5e33ebdb3114adc0b9b4991ffe18d5f44353
Raghava <raghava.tammu@xcubelabs.com>
2020-10-31 21:18:06 +0530

    added synce data with out duplicate and appending the description

--- app/Models/RiskAssessmentModel.php
+++ app/Models/RiskAssessmentModel.php
@@ -6,28 +6,6 @@ class RiskAssessmentModel extends Model{
     protected $table = 'docsgo-risks';
     protected $allowedFields = ['project_id', 'risk_type', 'risk', 'description', 'component', 'mitigation', 'baseScore_severity','status', 'assessment', 'update_date'];
 
-    function getRisksOld($status = ""){
-        $db      = \Config\Database::connect();
-        $whereCondition = "";
-        if($status != ""){
-            $whereCondition = " WHERE risks.status = '".$status."' ";
-        }
-        $sql = "SELECT
-        risks.id, proj.name as project, risks.risk_type, risks.risk, risks.description, risks.mitigation,  risks.rpn, risks.status, risks.update_date,
-         sev.`name` AS severity, occ.`name` AS occurrence, det.`name` AS detectability
-        FROM `docsgo-risks` risks
-        LEFT JOIN `docsgo-status-options` sev ON  sev.`value`=risks.`severity`
-        LEFT JOIN `docsgo-status-options` occ ON risks.`occurrence` = occ.`value`
-        LEFT JOIN `docsgo-status-options` det ON risks.`detectability` = det.`value`
-        LEFT JOIN `docsgo-projects` proj ON risks.`project_id` = proj.`project-id`
-        ".$whereCondition ."
-        ORDER BY update_date;";
-
-        $query = $db->query($sql);
-        $data = $query->getResult('array');
-
-        return $data;
-    }
 
     function getRisks($status = '', $type = '') {
         $db      = \Config\Database::connect();
@@ -50,14 +28,25 @@ class RiskAssessmentModel extends Model{
         $db = \Config\Database::connect();
 
         $whereCondition = " WHERE risk_type = 'Vulnerability' "; 
-        $sql = "SELECT * FROM `docsgo-risks`
-        ".$whereCondition ."
-        ORDER BY update_date;";
-
+        $sql = "SELECT * FROM `docsgo-risks` ". $whereCondition . "ORDER BY update_date;";
         $query = $db->query($sql);
         $data = $query->getResult('array');
         return $data;
     }
+
+    function updateVulnerabilityDescription( $riskId, $description){
+        $db = \Config\Database::connect();
+
+        $whereCondition = " WHERE id = ".$riskId." "; 
+        $sql = "UPDATE `docsgo-risks` SET `description` = '".$description."'
+        ".$whereCondition ."";
+
+        $query = $db->query($sql);
+        $data = $query->getResult('array');
+
+        return $data; 
+    }
+
 
     function getSonarRecords(){
         $settingsModel = new SettingsModel();
