a177d4ed3c3ae3c0832931fb92e142bbea120998
sudhanshu tiwari <sudhanshu@viosmedical.com>
2020-11-20 10:55:23 +0530

    scroll to top button in documents page

--- app/Views/ProjectDocuments/form.php
+++ app/Views/ProjectDocuments/form.php
@@ -38,6 +38,14 @@
         z-index:9;
     }
 
+    .back-to-top {
+        position: fixed;
+        bottom: 25px;
+        right: 25px;
+        display: none;
+        border: 1px solid;
+    }
+
 </style>
 
 <div class="row pl-4 justify-content-center pt-0">
@@ -405,7 +413,7 @@
     <?php endif; ?>
 </div>
 
-
+<a id="back-to-top" href="#" class="btn btn-light btn-lg back-to-top text-primary" role="button"><i class="fas fa-chevron-up"></i></a>
 <script>
     var existingDocList = [];
     var revisionHistory = null;
@@ -430,6 +438,21 @@
     var reviewedSection = [], reviewComments = "", reviewCategory = [], documentStatus = [];
 
     $(document).ready( function () {
+        $(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+        });
+        
         <?php if (isset($lookUpTables)): ?>
             lookUpTables = <?= json_encode($lookUpTables) ?>;
         <?php endif; ?>
