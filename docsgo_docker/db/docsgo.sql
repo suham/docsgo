2590ba70587d08c5f4949c4b4927b4fda1461d73
Raghava <raghava.tammu@xcubelabs.com>
2020-11-25 14:30:12 +0530

    1.Added column in projecttable, 2.Added new row in setting table and alter enums

--- docsgo_docker/db/docsgo.sql
+++ docsgo_docker/db/docsgo.sql
@@ -531,3 +531,10 @@ ALTER TABLE `docsgo-test-cases` MODIFY description longtext;
 CREATE TABLE `docsgo`.`docsgo-taskboard` ( `id` INT NOT NULL AUTO_INCREMENT , `task_column` VARCHAR(20) NOT NULL , `task_category` VARCHAR(20) NOT NULL , `description` TEXT NULL , `title` VARCHAR(200) NOT NULL , `project_id` INT NOT NULL , `assignee` INT NULL , `qa` INT NULL , `comments` JSON NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
 ALTER TABLE `docsgo-taskboard` CHANGE `qa` `verifier` INT(11) NULL DEFAULT NULL;
+
+ALTER TABLE `docsgo-projects` ADD COLUMN `download-path` varchar(100) AFTER name;
+
+ALTER TABLE `docsgo-settings` MODIFY COLUMN `type` enum('dropdown','url','properties') NOT NULL
+
+INSERT INTO `docsgo-settings` (type, identifier, options) VALUES ('properties','documentProperties','[{"key":"docTitle","value":""},{"key":"docIcon","value":""},{"key":"docConfidential","value":""}]');
+
